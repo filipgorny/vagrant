ebabfb9b9d72241b4d975d1bf814191d1863289e
Filip Górny <filip.gorny@gmail.com>
2016-05-08 18:04:19 +0200

    mysql

---
+++ modules/mysql/install.sh
@@ -0,0 +1,3 @@
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server
